ba7ea5933a39ebad28c800325cb5a07983ae78c0
Lizzie <lizzie@rectangle.design>
2021-03-04 08:55:54 +0000

    updates to theme

--- www/gsaopenstudioshowcase.net/wp-content/themes/gsaopenstudioshowcase/header.php
+++ www/gsaopenstudioshowcase.net/wp-content/themes/gsaopenstudioshowcase/header.php
@@ -63,7 +63,7 @@ $classes = get_body_class();
 			<p>Open Studio &mdash; Portfolio Preparation Course,<br/> Creative Practices 2020–2021</p>
 		</div>	
 		<h2><a href="<?php echo site_url();?>">We Lost You For A Moment There</a></h2>
-		<p class="essay">It gives us great pleasure to share the work of this year’s students who have studied with us on our Portfolio Preparation Course - Creative Practices. All have shown a prodigious amount of dedication to their studies during very challenging times and have embraced working online with us, establishing remote working spaces, with continued enthusiasm and commitment. They have all been remarkably abundant in their making and created portfolios of work that are diverse and engrossing; whilst supporting each other in their learning, being extraordinarily good natured and kind throughout.</p>
+		<p class="essay">It gives us great pleasure to share the work of this year’s students who have studied with us on our Portfolio Preparation Course – Creative Practices. All have shown a prodigious amount of dedication to their studies during very challenging times and have embraced working online with us, establishing remote working spaces, with continued enthusiasm and commitment. They have all been remarkably abundant in their making and created portfolios of work that are diverse and engrossing; whilst supporting each other in their learning, being extraordinarily good natured and kind throughout.</p>
 		<p class="essay"><a href=""><span>Continue Reading</span> &rarr;</a></p>
 	</article>
 </section>
